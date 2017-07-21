384e86a8647a22bbac8eaa9dd98f6f3d8289829d
Victor Shepardson <victor.shepardson@gmail.com>
2017-07-21 02:29:37 -0400

    jointly optimize encoder, generator; CIFAR notebook; fixes; README

--- README.md
+++ README.md
@@ -3,8 +3,46 @@ pytorch implementation of Rosca, Mihaela, et al. "Variational Approaches for Aut
 
 work in progress.
 
-## Generic
+## Deviations From The Paper
 
-supply any torch.nn.Module decoder, generator, discriminator, and code discriminator at construction and any torch.optim.Optimizer and torch.utils.DataLoader to fit()
+In the original paper, prior and posterior terms appear to be swapped in the code discriminator loss (equations 16 and 17 in Algorithm 1).
+
+Algorithm 1 in the paper is generally vague as to how each network should be updated. In this implementation:
+
+- Encoder and generator are trained jointly
+- Discriminator and code discriminator are trained jointly
+- Discriminators are updated before encoder/generator
+- Training alternates full passes through the data (as apposed to updating each network every batch)
 
 ## Examples
+
+alphagan/examples/CIFAR.ipynb
+
+## Usage
+
+```from alphagan import AlphaGAN
+
+encoder, generator, D, C = ... #torch.nn.Module
+
+model = AlphaGAN(encoder, generator, D, C, lambd=10, latent_dim=32)
+
+X_train, X_valid = ... #torch.utils.data.DataSet
+
+train_loader, valid_loader = ... #torch.utils.data.DataLoader
+
+model.fit(train_loader, valid_loader, n_epochs=80, log_fn=print)
+
+# encode and reconstruct
+z_valid, x_recon = model(X_valid[0])
+
+# sample from the generative model
+z, x_gen = model(batch_size, mode='sample')
+```
+
+### Generic
+
+Supply any torch.nn.Module decoder, generator, discriminator, and code discriminator at construction and any torch.optim.Optimizer and torch.utils.DataLoader to fit().
+
+### Progress Bars
+
+Install tqdm for progress bars. To get working nested progress bars in jupyter notebooks: `pip install -e git+https://github.com/dvm-shlee/tqdm.git@master#egg=tqdm`
--- alphagan/alphagan.py
+++ alphagan/alphagan.py
@@ -1,3 +1,6 @@
+from itertools import chain, repeat
+from collections import defaultdict
+
 import numpy as np
 
 import torch
@@ -17,6 +20,7 @@ def in_jupyter():
 
 try:
     if in_jupyter():
+        # from tqdm import tqdm as pbar
         from tqdm import tqdm_notebook as pbar
     else:
         from tqdm import tqdm as pbar
@@ -24,12 +28,26 @@ except ImportError:
     def pbar(it, *a, **kw):
         return it
 
-def _freeze(module):
-    for p in module.parameters():
-        p.requires_grad = False
-def _unfreeze(module):
-    for p in module.parameters():
-        p.requires_grad = True
+# avoid log(0)
+_eps = 1e-15
+
+def _freeze(*args):
+    for module in args:
+        for p in module.parameters():
+            p.requires_grad = False
+def _unfreeze(*args):
+    for module in args:
+        for p in module.parameters():
+            p.requires_grad = True
+
+def _take_epochs(X, n_epochs):
+    """Get a fractional number of epochs from X, rounded to the batch
+    X: torch.utils.DataLoader
+    n_epochs: number of iterations through the data.
+    """
+    n_batches = int(np.ceil(len(X)*n_epochs))
+    n_shuffles = int(np.ceil(n_epochs))
+    return islice(chain.from_iterable(repeat(X,n_shuffles)),n_batches)
 
 def _wrap(x):
     """ensure x is a Variable."""
@@ -60,83 +78,192 @@ class AlphaGAN(nn.Module):
         self.C = C
         self.latent_dim = latent_dim
         self.lambd = lambd
-        self.rec_loss = nn.L1Loss()
+        # self.rec_loss = nn.L1Loss()
 
     def sample_prior(self, n):
+        """Sample self.latent_dim-dimensional unit normal.
+        n: batch size
+        """
         return Variable(torch.randn(n, self.latent_dim), requires_grad=False)
 
-    def encoder_loss(self, x):
-        """Loss for step 4 of algorithm 1"""
-        _freeze(self)
-        _unfreeze(self.encoder)
-        z, x_rec = self(x)
-        return self.rec_loss(x_rec, x) - self.C(z).log().mean(0)
+    def rec_loss(self, x_rec, x):
+        """L1 reconstruction error or Laplace log likelihood"""
+        return self.lambd * (x_rec-x).abs().mean()
 
-    def generator_loss(self, x):
-        """Loss for step 5 of algorithm 1"""
-        _freeze(self)
-        _unfreeze(self.generator)
+    def autoencoder_loss(self, x):
+        """Return reconstruction loss, adversarial loss"""
         z_prior, x_prior = self(len(x), mode='sample')
         z, x_rec = self(x)
-        return self.rec_loss(x_rec, x) - (
-            self.D(x_rec).log()
-            + self.D(x_prior).log()
-        ).mean(0)
+        xs = torch.cat((x_prior, x_rec), 0)
+        return dict(
+            reconstruction = self.rec_loss(x_rec, x),
+            adversarial = -(self.D(xs) + _eps).log().mean(),
+            code = -(self.C(z) + _eps).log().mean()
+        )
 
-    def D_loss(self, x):
-        """Loss for step 6 of algorithm 1"""
-        _freeze(self)
-        _unfreeze(self.D)
+    def discriminator_loss(self, x):
+        """Return discriminator (D) loss, code discriminator (C) loss"""
         z_prior, x_prior = self(len(x), mode='sample')
         z, x_rec = self(x)
-        return - (
-            self.D(x).log()
-            + (1 - self.D(x_rec)).log()
-            + (1 - self.D(x_prior)).log()
-        ).mean(0)
+        xs = torch.cat((x_prior, x_rec), 0)
+        return dict(
+            D = - (self.D(x) + _eps).log().mean()
+                - (1 - self.D(xs) + _eps).log().mean(),
+            C = - (self.C(z_prior) + _eps).log().mean()
+                - (1 - self.C(z) + _eps).log().mean()
+        )
 
-    def C_loss(self, x):
-        """Loss for step 7 of algorithm 1"""
-        _freeze(self)
-        _unfreeze(self.C)
-        z_prior = self.sample_prior(len(x))
-        z = self.encoder(x)
-        return - (
-            self.C(z).log() + (1-self.C(z_prior)).log()
-        ).mean(0)
+    def fit_step(self, X, loss_fn, optimizer=None, n_epochs=1):
+        """Optimize for one epoch.
+        X: torch.utils.DataLoader
+        loss_fn: return dict of loss component Variables
+        optimizer: if None, just compute loss components (e.g. for validation)
+        n_epochs: fractional number of passes through the data
+        """
+        batch_losses = defaultdict(list)
+        loss_name = loss_fn.__name__
+        # train discriminator
+        for x in pbar(_take_epochs(X, n_epochs), desc=loss_name, leave=False):
+            if optimizer:
+                self.zero_grad()
+            loss_components = loss_fn(_wrap(x))
+            if optimizer:
+                loss = sum(loss_components.values())
+                loss.backward()
+                optimizer.step()
+            for k,v in loss_components.items():
+                batch_losses[k].append(v.data.cpu().numpy())
+        return {k+'_'+loss_name:np.mean(v) for k,v in batch_losses.items()}
 
     def fit(self,
             X_train, X_valid=None,
-            optim_fns=None,
+            disc_opt_fn=None, ae_opt_fn=None,
+            disc_epochs=.5, ae_epochs=1,
             log_fn=None,
             n_epochs=100):
+        """
+        X_train: torch.utils.data.DataLoader
+        X_valid: torch.utils.data.DataLoader or None
+        disc_opt_fn: takes parameter set, returns torch.optim.Optimizer
+            if None, a default Optimizer will be used
+        ae_opt_fn: takes parameter set, returns torch.optim.Optimizer
+            if None, a default Optimizer will be used
+        disc_epochs: number of discriminator passes through the data each epoch
+            (may be fractional)
+        ae_epochs: number of autoencoder passes through the data each epoch
+            (may be fractional)
+        log_fn: takes diagnostic dict, called after every epoch
+        n_epochs: number of discriminator, autoencoder training iterations
+        """
         _unfreeze(self)
-        optim_fns = optim_fns or [torch.optim.Adam]*4
-        optimizers = [opt(m.parameters()) for opt, m in zip(optim_fns, (
-            self.encoder, self.generator, self.D, self.C
-        ))]
-        loss_fns = [
-            self.encoder_loss, self.generator_loss, self.D_loss, self.C_loss
-        ]
+
+        disc_opt_fn = disc_opt_fn or (lambda p: torch.optim.Adam(p, lr=3e-4))
+        ae_opt_fn = ae_opt_fn or (lambda p: torch.optim.Adam(p, lr=3e-4))
+
+        disc_opt = disc_opt_fn(chain(
+            self.D.parameters(), self.C.parameters()))
+        ae_opt = ae_opt_fn(chain(
+            self.encoder.parameters(), self.generator.parameters()))
+
         for i in pbar(range(n_epochs), desc='epoch'):
-            diagnostic = {}
-            for optimizer, loss_fn in zip(optimizers, loss_fns):
-                batch_losses = []
-                it_name = loss_fn.__name__
-                # training
-                for x in pbar(X_train, desc=it_name, leave=False):
-                    optimizer.zero_grad()
-                    loss = loss_fn(_wrap(x))
-                    loss.backward()
-                    optimizer.step()
-                    batch_losses.append(loss.data.cpu().numpy())
-                diagnostic['train_'+it_name] = np.mean(batch_losses)
-                # validation
-                if not X_valid is None:
-                    diagnostic['valid_'+it_name] = np.mean([
-                        loss_fn(x).data.cpu().numpy() for x in X_valid ])
+            diagnostic = defaultdict(dict)
+
+            # train discriminator
+            _freeze(self.encoder, self.generator)
+            _unfreeze(self.C, self.D)
+            diagnostic['train'].update( self.fit_step(
+                X_train, self.discriminator_loss, disc_opt, disc_epochs ))
+
+            # train autoencoder
+            _freeze(self.C, self.D)
+            _unfreeze(self.encoder, self.generator)
+            diagnostic['train'].update( self.fit_step(
+                X_train, self.autoencoder_loss, ae_opt, ae_epochs ))
+
+            # validation
+            _freeze(self)
+            if not X_valid is None:
+                diagnostic['valid'].update( self.fit_step(
+                    X_valid, self.discriminator_loss ))
+                diagnostic['valid'].update( self.fit_step(
+                    X_valid, self.autoencoder_loss ))
+
             # log the dict of losses
             log_fn(diagnostic)
+
+    # def encoder_loss(self, x):
+    #     """Loss for step 4 of algorithm 1"""
+    #     z, x_rec = self(x)
+    #     return self.rec_loss(x_rec, x) - self.C(z).log().mean()
+    #
+    # def generator_loss(self,x):
+    #     """Loss for step 5 of algorithm 1"""
+    #     z_prior, x_prior = self(len(x), mode='sample')
+    #     z, x_rec = self(x)
+    #     return self.rec_loss(x_rec, x) - self.D(
+    #          torch.cat((x_prior, x_rec), 0)
+    #     ).log().mean()
+
+    # def D_loss(self, x):
+    #     """Loss for step 6 of algorithm 1"""
+    #     z_prior, x_prior = self(len(x), mode='sample')
+    #     z, x_rec = self(x)
+    #     return (
+    #         - self.D(x).log().mean()
+    #         - (1 - self.D( torch.cat((x_prior, x_rec), 0) )).log().mean()
+    #     )
+    #
+    # def C_loss(self, x):
+    #     """Loss for step 7 of algorithm 1"""
+    #     z_prior = self.sample_prior(len(x))
+    #     z = self.encoder(x)
+    #     return (
+    #         - self.C(z).log()
+    #         - (1-self.C(z_prior)).log()
+    #     ).mean()
+
+    # def fit(self,
+    #         X_train, X_valid=None,
+    #         optim_fns=None,
+    #         epoch_len=[1,2,1,1],
+    #         log_fn=None,
+    #         n_epochs=100):
+    #     _unfreeze(self)
+    #     optim_fns = optim_fns or [lambda p: torch.optim.Adam(p, lr=2e-4)]*4
+    #     modules = [self.encoder, self.generator, self.D, self.C]
+    #     optimizers = [opt(m.parameters()) for opt, m in zip(optim_fns, modules)]
+    #     loss_fns = [
+    #         self.encoder_loss, self.generator_loss, self.D_loss, self.C_loss
+    #     ]
+    #     for i in pbar(range(n_epochs), desc='epoch'):
+    #         diagnostic = {}
+    #         for mod, n, optimizer, loss_fn in zip(
+    #             modules, epoch_len, optimizers, loss_fns
+    #             ):
+    #             ################ DEBUG
+    #             if mod==self.D: continue
+    #             if mod==self.C: continue
+    #             ################
+    #             _freeze(self)
+    #             _unfreeze(mod)
+    #             batch_losses = []
+    #             it_name = loss_fn.__name__
+    #             # training
+    #             for x in pbar(
+    #                 _take_epochs(X_train,n), desc=it_name, leave=False
+    #                 ):
+    #                 self.zero_grad()
+    #                 loss = loss_fn(_wrap(x))
+    #                 loss.backward()
+    #                 optimizer.step()
+    #                 batch_losses.append(loss.data.cpu().numpy())
+    #             diagnostic['train_'+it_name] = np.mean(batch_losses)
+    #             # validation
+    #             if not X_valid is None:
+    #                 diagnostic['valid_'+it_name] = np.mean([
+    #                     loss_fn(x).data.cpu().numpy() for x in X_valid ])
+    #         # log the dict of losses
+    #         log_fn(diagnostic)
 
     def forward(self, *args, mode=None):
         """
